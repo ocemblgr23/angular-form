108e0ab4db154a7ce07200e4d95b45c086d607dc
ocemblgr23 <ocem.blgr2023@gmail.com>
2023-10-23 12:20:06 +0530

    implement full-width search bar

--- src/app/prime/prime.module.ts
+++ src/app/prime/prime.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
-
+import { InputTextModule } from 'primeng/inputtext';
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [
-    ButtonModule,
-    MenubarModule
-  ],
+  exports: [ButtonModule, MenubarModule, InputTextModule],
 })
 export class PrimeModule {}
